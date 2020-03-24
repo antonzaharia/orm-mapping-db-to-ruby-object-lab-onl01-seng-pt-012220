6d6d78c9de4634a5ae20086dfb4ecbe05ab1b0e4
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-03-24 15:12:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,8 @@ class Student
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade = 10 LIMIT ?
+    WHERE grade = '10'
+    LIMIT ?
     SQL
     
     DB[:conn].execute(sql, x).map do |row|
